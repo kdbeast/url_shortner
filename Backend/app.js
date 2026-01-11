7323eca451925321b59abddc2f5176cb6548140f
Karan Jamwal <karanjamwal2004@gmail.com>
2026-01-11 23:01:15 +0530

    jsonwebtoken implemented

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import authRoute from "./src/routes/auth.route.js";
 import connectDB from "./src/config/mongoose.config.js";
 import shortUrlRoute from "./src/routes/shortUrl.route.js";
 import { errorHandler } from "./src/utils/errorHandler.js";
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/auth", authRoute);
 app.use("/api/create", shortUrlRoute);
 app.get("/:id", redirectFromShortUrl);
 
--- Backend/src/utils/helper.js
+++ Backend/src/utils/helper.js
@@ -1,5 +1,15 @@
+import jwt from "jsonwebtoken";
 import { nanoid } from "nanoid";
 
-export const generateNanoId = (length) =>{
-    return nanoid(length);
-} ;
+export const generateNanoId = (length) => {
+  return nanoid(length);
+};
+
+export const signToken = (id) => {
+  return jwt.sign(id, process.env.JWT_SECRET, { expiresIn: "5m" });
+};
+
+export const verifyToken = (token) => {
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  console.log(decoded.id);
+};
